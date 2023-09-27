aa47bb403648a5bf03c20205cba98114adaa317d
LaylaR <layla.richard.2111@gmail.com>
2023-09-27 07:29:01 +0200

    Simplify generate card by using card template change collections to js objects array

--- assets/js/booksCollection.js
+++ assets/js/booksCollection.js
@@ -1,93 +1,103 @@
-﻿import {Book} from "./classes.js";
+﻿let imgPath = "assets/img/";
+let booksCollection=[
 
-let imgPath = "assets/img/";
-let booksCollection=[];
+    {
+        author: "John Connolly",
+        title:  "Every dead thing",
+        date: new Date(1999,0),
+        summary:"Introducing Detective Charlie Parker, whose hunt for a serial killer brings revelations.",
+        cover:`${imgPath}Tome1.png`, 
+        number:1,
+        readMoreLink: "https://www.johnconnollybooks.com/books/-every-dead-thing----------------------------"
+    },
+    {
+        author: "John Connolly",
+        title:  "Dark Hollow",
+        date: new Date(2000,0),
+        summary:"PI Charlie Parker seeks justice for a dead woman and her son in a wintry Maine town.",
+        cover:`${imgPath}Tome2.png`,
+        number:2,
+        readMoreLink: "https://www.johnconnollybooks.com/books/-dark-hollow---------------------------------"
+    },
+    {
+        author: "John Connolly",
+        title:  "The Killing Kind",
+        date: new Date(2001,0),
+        summary:"PI Charlie Parker finds ties between the murder of an old friend and a 40-year-old massacre.",
+        cover:`${imgPath}Tome3.png`,
+        number:3,
+        readMoreLink: "https://www.johnconnollybooks.com/books/-the-killing-kind----------------------------"
+        
+    },
+    {
+        author: "John Connolly",
+        title:  "The White Road",
+        date: new Date(2002,0),
+        summary:"Parker hunts a murderer in South Carolina with ties to an old adversary.",
+        cover:`${imgPath}Tome4.png`,
+        number:4,
+        readMoreLink: "https://www.johnconnollybooks.com/books/-the-white-road------------------------------"
+    },
+    {
+        author: "John Connolly",
+        title:  "The Black Angel",
+        date: new Date(2005,0),
+        summary:"Parker's search for the \"Black Angel\" relic leads to violence in this world and war in the next.",
+        cover:`${imgPath}Tome5.png`,
+        number:5,
+        readMoreLink: "https://www.johnconnollybooks.com/books/the-black-angel"
+    },
 
-let book1= new Book(
-    "John Connolly",
-    "Every dead thing",
-    new Date(1999,0),
-    "Introducing Detective Charlie Parker, whose hunt for a serial killer brings revelations.",
-    `${imgPath}Tome1.png`, 'Charlie Parker Serie',1,"https://www.johnconnollybooks.com/books/-every-dead-thing----------------------------");
+    {
+        author: "John Connolly",
+        title:  "The Unquiet",
+        date: new Date(2007,0),
+        summary:"Parker uncovers the terrible secrets of his client's father, a child psychiatrist.",
+        cover:`${imgPath}Tome6.png`,
+        number:6,
+        readMoreLink: "https://www.johnconnollybooks.com/books/-the-unquiet---------------------------------"
+    },
 
-let book2= new Book(
-    "John Connolly",
-    "Dark Hollow",
-    new Date(2000,0),
-    "PI Charlie Parker seeks justice for a dead woman and her son in a wintry Maine town.",
-    `${imgPath}Tome2.png`,'Charlie Parker Serie',2,"https://www.johnconnollybooks.com/books/-dark-hollow---------------------------------");
+    {
+        author: "John Connolly",
+        title: "The Reapers",
+        date: new Date(2007,0),
+        summary:"Parker's allies Louis and Angel confront a deadly vendetta rooted in Louis's past.",
+        cover:`${imgPath}Tome7.png`,
+        number:7,
+        readMoreLink: "https://www.johnconnollybooks.com/books/-the-reapers---------------------------------"
+    },
 
-let book3= new Book(
-    "John Connolly",
-    "The Killing Kind",
-    new Date(2001,0),
-    "PI Charlie Parker finds ties between the murder of an old friend and a 40-year-old massacre.",
-    `${imgPath}Tome3.png`,'Charlie Parker Serie',3,"https://www.johnconnollybooks.com/books/-the-killing-kind----------------------------");
 
-let book4= new Book(
-    "John Connolly",
-    "The White Road",
-    new Date(2002,0),
-    "Parker hunts a murderer in South Carolina with ties to an old adversary.",
-    `${imgPath}Tome4.png`,'Charlie Parker Serie',4,"https://www.johnconnollybooks.com/books/-the-white-road------------------------------");
+    {
+        author: "John Connolly",
+        title: "The Lovers",
+        date: new Date(2009,0),
+        summary:"The hunt for a young woman leads to revelations about Parker's own father.",
+        cover:`${imgPath}Tome8.png`,
+        number:8,
+        readMoreLink: "https://www.johnconnollybooks.com/books/the-lovers"
+    },
+    {
+        author: "John Connolly",
+        title: "The Whisperers",
+        date: new Date(2010,0),
+        summary:"Parker joins forces with an old enemy to stop a deadly and mysterious smuggling operation.",
+        cover:`${imgPath}Tome9.png`,
+        number:9,
+        readMoreLink: "https://www.johnconnollybooks.com/books/-the-whisperers------------------------------"
+    },
+    {
+        author: "John Connolly",
+        title: "The Burning Soul",
+        date: new Date(2011,0),
+        summary:"Parker hunts a missing girl whose disappearance is linked to a long-kept secret.",
+        cover:`${imgPath}Tome10.png`,
+        number:10,
+        readMoreLink: "https://www.johnconnollybooks.com/books/-the-burning-soul----------------------------"
+    }
 
-let book5= new Book(
-    "John Connolly",
-    "The Black Angel",
-    new Date(2005,0),
-    "Parker's search for the \"Black Angel\" relic leads to violence in this world and war in the next.",
-    `${imgPath}Tome5.png`,'Charlie Parker Serie',5,"https://www.johnconnollybooks.com/books/the-black-angel");
+];
 
-let book6= new Book(
-    "John Connolly",
-    "The Unquiet",
-    new Date(2007,0),
-    "Parker uncovers the terrible secrets of his client's father, a child psychiatrist.",    
-    `${imgPath}Tome6.png`,
-    'Charlie Parker Serie',
-    6,
-    "https://www.johnconnollybooks.com/books/-the-unquiet---------------------------------");
-
-let book7= new Book(
-    "John Connolly",
-    "The Reapers",
-    new Date(2007,0),
-    "Parker's allies Louis and Angel confront a deadly vendetta rooted in Louis's past.",
-    `${imgPath}Tome7.png`,
-    'Charlie Parker Serie',
-    7,
-    "https://www.johnconnollybooks.com/books/-the-reapers---------------------------------");
-
-let book8= new Book(
-    "John Connolly",
-    "The Lovers",
-    new Date(2009,0),
-    "The hunt for a young woman leads to revelations about Parker's own father.",
-    `${imgPath}Tome8.png`,
-    'Charlie Parker Serie',
-    8,
-    "https://www.johnconnollybooks.com/books/the-lovers");
-
-let book9= new Book(
-    "John Connolly",
-    "The Whisperers",
-    new Date(2010,0),
-    "Parker joins forces with an old enemy to stop a deadly and mysterious smuggling operation.",
-    `${imgPath}Tome9.png`,
-    'Charlie Parker Serie',
-    9,
-    "https://www.johnconnollybooks.com/books/-the-whisperers------------------------------");
-
-let book10= new Book(
-    "John Connolly",
-    "The Burning Soul",
-    new Date(2011,0),
-    "Parker hunts a missing girl whose disappearance is linked to a long-kept secret.",
-    `${imgPath}Tome10.png`,
-    'Charlie Parker Serie',
-    10,
-    "https://www.johnconnollybooks.com/books/-the-burning-soul----------------------------");
-
-booksCollection.push(book1,book2,book3,book4,book5,book6,book7,book8,book9,book10);
 
 export{booksCollection};
