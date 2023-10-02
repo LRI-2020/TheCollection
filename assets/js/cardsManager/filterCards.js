4c5a3f830885995160a1f5f0f2de76f50f7042d8
LaylaR <layla.richard.2111@gmail.com>
2023-10-02 17:00:46 +0200

    xss attacks and solutions

--- assets/js/cardsManager/filterCards.js
+++ assets/js/cardsManager/filterCards.js
@@ -1,5 +1,6 @@
 ﻿import {booksCollection} from "../booksCollection.js";
 import {DisplayCards} from "./cardDisplayer.js";
+
 // import * as DOMPurify from '../../dompurify/';
 
 function FilterCardsByDate(year) {
@@ -22,33 +23,69 @@ function FilterCardsByDate(year) {
 
 }
 
-function DisplayFilter(filter){
+function dangerToSafe(character) {
     
+    switch(character){
+        case "<":
+            character = "&lt;"
+            break;
+        case">":
+            character = "&gt;"
+            break;
+        case'"':
+            character = "&quot;"
+            break;
+        case"'":
+            character = "&apos;"
+            break;
+        case'&':
+            character = "&amp;"
+            break;
+    }
+    
+    return character;
+}
+
+function encodeFilter(filter) {
+
+    let arr = filter.split("");
+    let safeArr = arr.map(i => dangerToSafe(i));
+    return safeArr.join("");
+}
+
+function DisplayFilter(filter) {
+
     let title = document.querySelector(".section-title__filter");
-    
+
     //Vulnerable code
 
-   // title.innerHTML=filter;
-    
-    //XSS attacks
-    //Attack 1 --  <img src="x" onerror="alert(document.cookie)" >
+   title.innerHTML=filter;
+
+    //XSS ATTACKS
+    // Attack 1 --  <img src="x" onerror="alert(document.cookie)" >
     // Attack 2 -- <button onmouseover=alert('Wufff!')>click me!</button>
- 
-    
-    //Solution 1 if content is only text
+
+
+    // SOLUTION 1 - USE SAFE SINK
     //title.innerText=filter;
     //OR
     //title.textContent=filter;
+
+    // SOLUTION 2 - ENCODE OUTPUT (better to use an external library for that)
+    // title.innerHTML = encodeFilter(filter);
+
+    // SOLUTION 3 - SANITIZE
+    // recommended library : DOMPurify
+    // DOMPurify.sanitize(filter);
+    // title.innerHTML=filter;
+
+
     
-    //Solution 2 - if innerHtml is needed - sanitize
-   // recommended library : DOMPurify
-   //  DOMPurify.sanitize(filter);
-    title.innerHTML=filter;
 }
 
 function RunFilter(params) {
     if (params !== undefined && params.has("year")) {
-    let value = params.get("year");
+        let value = params.get("year");
         let filteredCards = FilterCardsByDate(value);
         DisplayCards(filteredCards);
         DisplayFilter(value);
